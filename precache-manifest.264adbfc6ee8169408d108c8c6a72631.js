cc25bf0f664b1ede194f23b33dd6fe865ebd554b
شاةثي <@.>
2019-07-27 19:39:17 +0200

    Updates

---
+++ precache-manifest.264adbfc6ee8169408d108c8c6a72631.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4fe9ab708f89f627a5b05d870cd3edc",
+    "url": "/weatherapp-with-react/index.html"
+  },
+  {
+    "revision": "122eaaea5568a813ee03",
+    "url": "/weatherapp-with-react/static/css/main.6df83e78.chunk.css"
+  },
+  {
+    "revision": "e083292f352b8a48b197",
+    "url": "/weatherapp-with-react/static/js/2.61ebbd48.chunk.js"
+  },
+  {
+    "revision": "122eaaea5568a813ee03",
+    "url": "/weatherapp-with-react/static/js/main.4433dce0.chunk.js"
+  },
+  {
+    "revision": "654003eb5fdc27e36748",
+    "url": "/weatherapp-with-react/static/js/runtime~main.5851d5d8.js"
+  }
+]);
