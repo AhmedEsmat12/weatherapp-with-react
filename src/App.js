add40af2952b98ebf8b7b84aca94bfc5f260e955
شاةثي <@.>
2019-07-27 19:51:46 +0200

    weatherapp

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
     e.preventDefault();
     const city=e.target.elements.city.value;
     const country=e.target.elements.country.value;
-    const api=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=e36ed364400282e43250b6c4c0274d44`)
+    const api=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=e36ed364400282e43250b6c4c0274d44`)
     const data=await api.json();
     console.log(data);
     if(city&&country)
